a9687aea119574fa4932fc93b013a80f9ae1db66
nelermont <nelermont@yandex.ru>
2020-11-26 12:39:39 +0300

    Initial Commit

---
+++ Micro Beat Machine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Micro Beat Machine
+//
+//  Created by Дмитрий Подольский on 26.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
